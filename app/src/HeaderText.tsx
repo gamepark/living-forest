e8ff6bfc29b5f67304bda182f09fa96867d95b60
hasiolba <laetitia.decoudu@yahoo.com>
2022-07-08 19:10:52 +0200

    First steps with header

--- app/src/HeaderText.tsx
+++ app/src/HeaderText.tsx
@@ -1,6 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import GameView from '@gamepark/living-forest/GameView'
 import {getPlayerName} from '@gamepark/living-forest/LivingForestOptions'
+import { getAnimalsType } from '@gamepark/living-forest/material/GuardianAnimalDetails'
+import Phase from '@gamepark/living-forest/Phase'
+import { getPlayer } from '@gamepark/living-forest/PlayerView'
 import {usePlayerId} from '@gamepark/react-client'
 import {useTranslation} from 'react-i18next'
 
@@ -9,9 +12,21 @@ type Props = {
   game?: GameView
 }
 
-export default function HeaderText({loading}: Props) {
+export default function HeaderText({loading, game}: Props) {
   const {t} = useTranslation()
   const playerId = usePlayerId()
   if (loading) return <>{t('Game loading...')}</>
+  if(game != null){
+    const player = getPlayer(game, playerId)
+    if (game?.phase == Phase.GuardianAnimals) return <>{t('Form the Guardian Animal Help Line. Draw a card.')}</>
+    if (game?.phase == Phase.Action) {
+      if(getAnimalsType(player.line) > 2){
+        return <>{t('You have 1 action.')}</>
+      }else{
+        return <>{t('You have 2 actions.')}</>
+      }
+    }
+  }
+
   return <>Loaded! Now what? Your player id is {getPlayerName(playerId, t)}</>
 }
