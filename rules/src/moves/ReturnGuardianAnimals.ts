4935dc2e96cd093689f1736f7d940ded44c1a034
hasiolba <laetitia.decoudu@yahoo.com>
2022-08-26 19:12:27 +0200

    Calculation victory points correction

--- rules/src/moves/ReturnGuardianAnimals.ts
+++ rules/src/moves/ReturnGuardianAnimals.ts
@@ -1,6 +1,8 @@
 import GameState from '../GameState'
 import GameView from '../GameView'
 import MoveType from './MoveType'
+import { getVictoryTilesCount } from '../material/VictoryTile';
+import Victory from '../material/Victory';
 
 type ReturnGuardianAnimals = {
   type: MoveType.ReturnGuardianAnimals
@@ -14,6 +16,7 @@ export function returnGuardianAnimals(state: GameState | GameView, _move: Return
     while (player.line.length > 0) {
       player.discard.push(player.line.shift()!)
     }
+    player.victory[2] = getVictoryTilesCount(player.victoryTiles, Victory.SacredFlower)
   })
 }
 
