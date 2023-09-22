362d39859851cd96516a6bfd3260b64acce3adbd
Patrick Beaumé <p.beaume@gmail.com>
2023-09-22 14:27:48 +0200

    little refacto on sacred tree attack

--- rules/src/refacto/rules/OnibiAttacksSacredTreeRule.ts
+++ rules/src/refacto/rules/OnibiAttacksSacredTreeRule.ts
@@ -8,20 +8,21 @@ import { Fire } from '../../material/Fire'
 export class OnibiAttacksSacredTreeRule extends MaterialRulesPart {
 
   onRuleStart() {
-    const takenLevel1Cards = CARDS_PER_ROW - this.material(MaterialType.GuardianAnimalCard).location(LocationType.ReserveRow).locationId(1).length
-    const takenLevel2Cards = CARDS_PER_ROW - this.material(MaterialType.GuardianAnimalCard).location(LocationType.ReserveRow).locationId(2).length
-    const takenLevel3Cards = CARDS_PER_ROW - this.material(MaterialType.GuardianAnimalCard).location(LocationType.ReserveRow).locationId(3).length
+    const reserve = this.material(MaterialType.GuardianAnimalCard).location(LocationType.ReserveRow)
+    const takenLevel1Cards = CARDS_PER_ROW - reserve.locationId(1).length
+    const takenLevel2Cards = CARDS_PER_ROW - reserve.locationId(2).length
+    const takenLevel3Cards = CARDS_PER_ROW - reserve.locationId(3).length
 
     const fireOnCircle = this.fireOnCircle.length
     const moves = []
     if (fireOnCircle < 7) {
+      const fireStack = this.material(MaterialType.FireTile).location(LocationType.FireStack)
       let max = 7 - fireOnCircle
 
       if (takenLevel1Cards) {
         const tokens = Math.min(takenLevel1Cards, max)
         moves.push(
-          ...this.material(MaterialType.FireTile)
-            .location(LocationType.FireStack)
+          ...fireStack
             .locationId(Fire.Fire2)
             .moveItems({ location: { type: LocationType.CircleOfSpiritBoardFire, id: Fire.Fire2 }}, tokens)
         )
@@ -31,8 +32,7 @@ export class OnibiAttacksSacredTreeRule extends MaterialRulesPart {
       if (max && takenLevel2Cards) {
         const tokens = Math.min(takenLevel2Cards, max)
         moves.push(
-          ...this.material(MaterialType.FireTile)
-            .location(LocationType.FireStack)
+          ...fireStack
             .locationId(Fire.Fire3)
             .moveItems({ location: { type: LocationType.CircleOfSpiritBoardFire, id: Fire.Fire3 }}, tokens)
         )
@@ -42,8 +42,7 @@ export class OnibiAttacksSacredTreeRule extends MaterialRulesPart {
       if (max && takenLevel3Cards) {
         const tokens = Math.min(takenLevel3Cards, max)
         moves.push(
-          ...this.material(MaterialType.FireTile)
-            .location(LocationType.FireStack)
+          ...fireStack
             .locationId(Fire.Fire4)
             .moveItems({ location: { type: LocationType.CircleOfSpiritBoardFire, id: Fire.Fire4 }}, tokens)
         )
