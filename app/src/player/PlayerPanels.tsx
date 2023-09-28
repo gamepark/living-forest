be83ac0786e460e935c52772eb45fbf59f9cd028
Patrick Beaumé <p.beaume@gmail.com>
2023-09-28 15:14:46 +0200

    spread panels at 4 players

--- app/src/player/PlayerPanels.tsx
+++ app/src/player/PlayerPanels.tsx
@@ -1,24 +1,26 @@
 /** @jsxImportSource @emotion/react */
-import { usePlayers } from '@gamepark/react-game'
+import { usePlayerId, usePlayers } from '@gamepark/react-game'
 import { FC } from 'react'
 import { css } from '@emotion/react'
 import { LivingForestPlayerPanel } from './LivingForestPlayerPanel'
+import SpiritOfNature from '@gamepark/living-forest/SpiritOfNature'
 
 export const PlayerPanels: FC = () => {
   const players = usePlayers({ sortFromMe: true })
+  const playerId = usePlayerId()
   return (
     <>
       {players.map((player, index) =>
-        <LivingForestPlayerPanel key={player.id} player={player} css={panelPosition(index, players.length)} />
+        <LivingForestPlayerPanel key={player.id} player={player} css={panelPosition(index, players.length, playerId)} />
       )}
     </>
   )
 }
 
-const panelPosition = (index: number, players: number) => css`
+const panelPosition = (index: number, players: number, player?: SpiritOfNature) => css`
   position: absolute;
   right: 1em;
-  top: ${8.5 + (index) * 19.5}em;
+  top: ${8.5 + (players < 4? ((index) * 19.5): ((player === undefined ? index : (index || players) - 1) * 76.5 / (players - 1)))}em;
   width: ${players < 4? 34: 28}em;
-  height: ${players < 4? 17.5: 14}em;
+  height: ${players < 4? 17.5: 17.5}em;
 `
