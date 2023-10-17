4bee89983c41ac6da19027955d27849a8d98f166
Patrick Beaumé <p.beaume@gmail.com>
2023-10-17 17:00:23 +0200

    fix: only shine the top fragment tile

--- app/src/material/description/FragmentTileDescription.tsx
+++ app/src/material/description/FragmentTileDescription.tsx
@@ -1,6 +1,9 @@
-import { TokenDescription } from '@gamepark/react-game'
+import { ItemContext, TokenDescription } from '@gamepark/react-game'
 import Images from '../../images/Images'
 import { FragmentTileRules } from './FragmentTileRules'
+import { isMoveItemType, MaterialMove } from '@gamepark/rules-api'
+import { MaterialType } from '@gamepark/living-forest/material/MaterialType'
+import { LocationType } from '@gamepark/living-forest/material/LocationType'
 
 export class FragmentTileDescription extends TokenDescription {
   width = 3
@@ -8,6 +11,22 @@ export class FragmentTileDescription extends TokenDescription {
   image = Images.fragment
 
   rules = FragmentTileRules
+
+  canDrag(move: MaterialMove, context: ItemContext): boolean {
+    return super.canDrag(move, context) && this.isTopItem(move, context)
+  }
+
+  canLongClick(move: MaterialMove, context: ItemContext): boolean {
+    return super.canLongClick(move, context) && this.isTopItem(move, context)
+  }
+
+  isTopItem(move: MaterialMove, context: ItemContext): boolean {
+    // Only compute it for element that goes to player fragment token
+    if (!isMoveItemType(MaterialType.FragmentTile)(move) || move.position.location?.type !== LocationType.PlayerFragmentTileStack) return true
+    const  { displayIndex, index, rules, type } = context
+    const shineIndex = (Math.min(rules.material(type).getItem(index)?.quantity ?? 0, 10) - 1)
+    return displayIndex === shineIndex
+  }
 }
 
 export const fragmentTileDescription = new FragmentTileDescription()
