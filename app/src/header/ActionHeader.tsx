96b57ed7b73471f3aa789d8f11b36c8cff305be2
Patrick Beaumé <p.beaume@gmail.com>
2023-10-18 10:40:36 +0200

    add different header for action header

--- app/src/header/ActionHeader.tsx
+++ app/src/header/ActionHeader.tsx
@@ -1,16 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import { useTranslation } from 'react-i18next'
-import { usePlayerId, useRules } from '@gamepark/react-game'
+import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { LivingForestRules } from '@gamepark/living-forest/LivingForestRules'
 import { Memory } from '@gamepark/living-forest/rules/Memory'
 
 export const ActionHeader = () => {
   const { t } = useTranslation()
   const rules = useRules<LivingForestRules>()!
+  const activePlayer = rules.getActivePlayer()
   const player = usePlayerId()
+  const name = usePlayerName(activePlayer)
   if (player === rules.getActivePlayer()) {
-    return <>{t('header.actions', { action: rules.remind(Memory.Actions) })} </>
+    return <>{t('header.actions.me', { action: rules.remind(Memory.Actions) })} </>
   }
 
-  return <>{t('header.actions-opponent', { action: rules.remind(Memory.Actions) })} </>
+  return <>{t('header.actions', { action: rules.remind(Memory.Actions), player: name })} </>
 }
