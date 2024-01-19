a1d9a938f0ca01679e5a168d486503b6dfeaa1d3
Patrick Beaume <p.beaume@gmail.com>
2024-01-19 15:39:30 +0100

    add short click on help line

--- app/src/locator/description/HelpLineDescription.tsx
+++ app/src/locator/description/HelpLineDescription.tsx
@@ -4,7 +4,7 @@ import SpiritOfNature from '@gamepark/living-forest/SpiritOfNature'
 import { MaterialType } from '@gamepark/living-forest/material/MaterialType'
 import { LocationType } from '@gamepark/living-forest/material/LocationType'
 import { css } from '@emotion/react'
-import { Location } from '@gamepark/rules-api'
+import { isMoveItemType, isSameLocationArea, Location, MaterialMove } from '@gamepark/rules-api'
 import { guardianAnimalCardDescription } from '../../material/description/GuardianAnimalCardDescription'
 import { getPlayerBoardPositionOnTable } from '../../utils/PositionOnTable'
 import { HelpLineRules } from './HelpLineRules'
@@ -31,6 +31,14 @@ export class HelpLineDescription extends LocationDescription<SpiritOfNature, Mat
       y: boardPosition.y - 13,
       z: 0
     }
+  }
+
+  canShortClick(move: MaterialMove, location: Location, context: MaterialContext): boolean {
+    if (isMoveItemType(MaterialType.GuardianAnimalCard)(move)) {
+      return isSameLocationArea(move.location, location)
+    }
+
+    return super.canShortClick(move, location, context)
   }
 
   help = HelpLineRules
