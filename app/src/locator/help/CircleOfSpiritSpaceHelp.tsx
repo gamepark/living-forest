9e3aa96e2c1e31b4cea6f34be32deb7532700e6e
hasiolba <laetitia.decoudu@yahoo.com>
2024-03-11 22:48:53 +0100

    Adding forest and circle rocks descriptions

--- app/src/locator/help/CircleOfSpiritSpaceHelp.tsx
+++ app/src/locator/help/CircleOfSpiritSpaceHelp.tsx
@@ -1,8 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import { FC } from 'react'
-import { LocationHelpProps } from '@gamepark/react-game'
+import { LocationHelpProps, PlayMoveButton, useLegalMoves } from '@gamepark/react-game'
+import { Trans, useTranslation } from 'react-i18next'
+import { MaterialMove, isCustomMoveType } from '@gamepark/rules-api'
+import { CustomMoveType } from '@gamepark/living-forest/rules/CustomMoveType'
 
-export const CircleOfSpiritSpaceHelp: FC<LocationHelpProps> = () => {
+export const CircleOfSpiritSpaceHelp: FC<LocationHelpProps> = (({ location, closeDialog }) => {
+  const { t } = useTranslation()
+  const legalMoves = useLegalMoves<MaterialMove>()
+  const move = legalMoves.find((move) => isCustomMoveType(CustomMoveType.MoveOnCircleOfSpirit)(move))
   return <>
+    <h2>{t('rules.circle-spirit-space.title')}</h2>
+    {(location.x == 1 || location.x == 3 || location.x == 5 || location.x == 7 || location.x == 9 || location.x == 11) &&
+      <p>{t('rules.circle-spirit-space.fragment')}</p>
+    }
+    {(location.x == 4 || location.x == 10) &&
+      <p>{t('rules.circle-spirit-space.extinguish')}</p>
+    }
+    {(location.x == 2 || location.x == 8) &&
+      <p>{t('rules.circle-spirit-space.attract')}</p>
+    }
+    {(location.x == 0 || location.x == 6) &&
+      <p>{t('rules.circle-spirit-space.plant')}</p>
+    }
+    {move && <hr />}
+    {move && <Trans defaults="rules.circle-spirit-space.button">
+      <PlayMoveButton move={move} onPlay={closeDialog} />
+    </Trans>}
   </>
-}
+})
--- app/src/locator/help/TreeSpaceHelp.tsx
+++ app/src/locator/help/TreeSpaceHelp.tsx
@@ -1,8 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import { LocationHelpProps } from '@gamepark/react-game'
 import { FC } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import { ResourceImage, resourceStyle } from '../../material/description/help/GuardianAnimalCardHelp'
 
-export const TreeSpaceHelp: FC<LocationHelpProps> = () => {
+export const TreeSpaceHelp: FC<LocationHelpProps> = ({ location }) => {
+  const { t } = useTranslation()
+  console.log(location);
   return <>
+    <h2>{t('rules.forest-space.title')}</h2>
+    <p>{t('rules.forest-space.plant')}</p>
+    {((location.x == 4 && location.y == 0) || (location.x == 0 && location.y == 2)) &&
+      <p>{t('rules.forest-space.fragments')}</p>
+    }
+    {((location.x == 4 && location.y == 2)) &&
+      <Trans defaults="rules.forest-space.extinguish">
+        <span css={resourceStyle(ResourceImage[2])} />
+      </Trans>
+    }
+    {((location.x == 0 && location.y == 0)) &&
+      <Trans defaults="rules.forest-space.attract">
+        <span css={resourceStyle(ResourceImage[1])} />
+      </Trans>
+    }
   </>
 }
