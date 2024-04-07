34e31ca2330c584be2a7de99753903ffcf9e9080
Laëtitia Découdu <60862340+hasiolba@users.noreply.github.com>
2024-04-08 01:21:57 +0200

    Adapt protective trees space

--- app/src/locator/ProtectiveTreeDeckLocator.tsx
+++ app/src/locator/ProtectiveTreeDeckLocator.tsx
@@ -8,12 +8,12 @@ export class ProtectiveTreeDeckLocator extends GridLocator {
   delta = { x: 0.5, y: 0.5, z: 0.5 }
 
   getColumns(_item: MaterialItem, { rules: { players } }: ItemContext): number {
-    return players.length < 3? 4: 3
+    return players.length < 3 ? 4 : 3
   }
 
   getCoordinates(item: MaterialItem, context: ItemContext) {
-    const { rules, displayIndex} = context
-    const { players} = rules
+    const { rules, displayIndex } = context
+    const { players } = rules
     const countTreeOfTypes = rules
       .material(MaterialType.ProtectiveTreeTiles)
       .location(LocationType.TreeDispenser)
@@ -22,9 +22,9 @@ export class ProtectiveTreeDeckLocator extends GridLocator {
 
     const selected = item.selected && (countTreeOfTypes - 1 === displayIndex)
     if (players.length < 3) {
-      return { x: -25, y: selected? -10: -8.5, z: 0}
+      return { x: -25, y: selected ? -10 : -16.5, z: 0 }
     }
 
-    return { x: 13.5, y: selected? 1: 2.5, z: 0}
+    return { x: 13.5, y: selected ? 1 : -5.5, z: 0 }
   }
 }
